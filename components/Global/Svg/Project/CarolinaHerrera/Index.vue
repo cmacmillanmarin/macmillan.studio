38f89daef211c528610f740fed6cfbe7992e2529
Christian MacMillan <christian@macmillan.studio>
2025-01-07 11:47:52 +0100

    selected project titles

--- components/Global/Svg/Project/CarolinaHerrera/Index.vue
+++ components/Global/Svg/Project/CarolinaHerrera/Index.vue
@@ -1,1 +1,124 @@
-<template></template>
+<template>
+  <div
+    :class="[
+      'svg__project__carolina-herrera',
+      { 'svg__project__carolina-herrera--animation': animation },
+    ]">
+    <template v-if="isMobileLayout">
+      <Ticker
+        ref="firstLineTickerEl"
+        :drag-on-target="true"
+        :ignore-update-scroll="true"
+        :ticker="!next ? nextProjectTickerFirstLine : undefined"
+        class="svg__project__carolina-herrera__ticker"
+        @update="emit('update-scroll')">
+        <div v-for="i in 2" :key="i">
+          <SvgProjectCarolinaHerreraFirstLine
+            v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+        </div>
+      </Ticker>
+      <Ticker
+        ref="secondLineTickerEl"
+        :drag-on-target="true"
+        :ignore-update-scroll="true"
+        :ticker="!next ? nextProjectTickerSecondLine : undefined"
+        :starting-direction="1"
+        class="svg__project__carolina-herrera__ticker"
+        @update="emit('update-scroll')">
+        <div v-for="i in 2" :key="i">
+          <SvgProjectCarolinaHerreraSecondLine
+            v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+        </div>
+      </Ticker>
+    </template>
+    <template v-else>
+      <SvgProjectCarolinaHerreraFirstLine
+        v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+      <SvgProjectCarolinaHerreraSecondLine
+        v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { shuffleIn } from '~/utils/animations'
+import Ticker from '~/components/Global/Ticker.vue'
+import { type NextProjectTicker } from '~/types/front/store'
+
+const props = defineProps<{
+  next: boolean
+  animation: boolean
+  color?: string
+}>()
+
+const id = 'project-ticker-carolina-herrera'
+
+const nextProjectTickerFirstLine = ref<NextProjectTicker | undefined>(
+  window.localStorage.getItem(`${id}-first-line`)
+    ? JSON.parse(window.localStorage.getItem(`${id}-first-line`) as string)
+    : undefined
+)
+const nextProjectTickerSecondLine = ref<NextProjectTicker | undefined>(
+  window.localStorage.getItem(`${id}-second-line`)
+    ? JSON.parse(window.localStorage.getItem(`${id}-second-line`) as string)
+    : undefined
+)
+
+const { isMobileLayout } = useDevice()
+
+const firstLineTickerEl = ref<typeof Ticker>()
+const secondLineTickerEl = ref<typeof Ticker>()
+
+const fill = computed(() => props.color || 'var(--black)')
+
+onBeforeUnmount(() => {
+  if (props.next && firstLineTickerEl.value && secondLineTickerEl.value) {
+    firstLineTickerEl.value.pause()
+    secondLineTickerEl.value.pause()
+    window.localStorage.setItem(
+      `${id}-first-line`,
+      JSON.stringify(firstLineTickerEl.value.getTicker())
+    )
+    window.localStorage.setItem(
+      `${id}-second-line`,
+      JSON.stringify(secondLineTickerEl.value.getTicker())
+    )
+  }
+})
+
+const emit = defineEmits(['update-scroll'])
+</script>
+
+<style lang="scss">
+.svg__project__carolina-herrera {
+  &--animation {
+    svg {
+      > path,
+      > g {
+        @include will-fade;
+      }
+    }
+  }
+
+  &__ticker {
+    margin-bottom: var(--layout-gutter);
+    > div {
+      padding-right: toScale(3.2rem, 37.5rem);
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  &__second-line {
+    @include from__tablet--landscape {
+      margin-top: toScale(1.2rem);
+      margin-left: 35.5vw;
+    }
+  }
+
+  svg path {
+    fill: v-bind(fill);
+  }
+}
+</style>
--- components/Global/Svg/Project/OurPlanet/Index.vue
+++ components/Global/Svg/Project/OurPlanet/Index.vue
@@ -1,1 +1,99 @@
-<template></template>
+<template>
+  <div :class="['svg__project__our-planet', { 'svg__project__our-planet--animation': animation }]">
+    <template v-if="isMobileLayout">
+      <SvgProjectOurPlanetFirstLine
+        v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+      <Ticker
+        ref="tickerEl"
+        :drag-on-target="true"
+        :ignore-update-scroll="true"
+        :ticker="!next ? nextProjectTicker : undefined"
+        class="svg__project__our-planet__ticker"
+        @update="emit('update-scroll')">
+        <div v-for="i in 2" :key="i">
+          <SvgProjectOurPlanetSecondLine
+            v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+        </div>
+      </Ticker>
+    </template>
+    <template v-else>
+      <SvgProjectOurPlanetFirstLine
+        v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+      <SvgProjectOurPlanetSecondLine
+        v-transition:in="{ callback: animation ? shuffleIn : () => {} }" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { shuffleIn } from '~/utils/animations'
+import Ticker from '~/components/Global/Ticker.vue'
+import { type NextProjectTicker } from '~/types/front/store'
+
+const props = defineProps<{
+  next: boolean
+  animation: boolean
+  color?: string
+}>()
+
+const id = 'project-ticker-our-planet'
+
+const nextProjectTicker = ref<NextProjectTicker | undefined>(
+  window.localStorage.getItem(id)
+    ? JSON.parse(window.localStorage.getItem(id) as string)
+    : undefined
+)
+
+const { isMobileLayout } = useDevice()
+
+const tickerEl = ref<typeof Ticker>()
+
+const fill = computed(() => props.color || 'var(--black)')
+
+onBeforeUnmount(() => {
+  if (props.next && tickerEl.value) {
+    tickerEl.value.pause()
+    window.localStorage.setItem(id, JSON.stringify(tickerEl.value.getTicker()))
+  }
+})
+
+const emit = defineEmits(['update-scroll'])
+</script>
+
+<style lang="scss">
+.svg__project__our-planet {
+  &--animation {
+    svg {
+      > path,
+      > g {
+        @include will-fade;
+      }
+    }
+  }
+
+  &__first-line {
+    margin: auto;
+    @include from__tablet--landscape {
+      margin: 0;
+      margin-bottom: toScale(1.2rem);
+    }
+  }
+
+  &__second-line {
+    @include from__tablet--landscape {
+      margin-left: 24vw;
+    }
+  }
+
+  &__ticker {
+    margin-top: var(--layout-gutter);
+    > div {
+      padding-right: toScale(3.2rem, 37.5rem);
+    }
+  }
+
+  svg path {
+    fill: v-bind(fill);
+  }
+}
+</style>
